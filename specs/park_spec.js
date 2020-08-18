5edcc0c4a7495b2841d33cc1087d4c56f1c1149c
Duncan Ryan <duncancryan1@gmail.com>
2020-08-18 23:07:50 +0100

    test to find top dino written and failing

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -41,7 +41,10 @@ describe('Park', function() {
     assert.deepStrictEqual(actual, [dinosaur1]);
   });
 
-  it('should be able to find the dinosaur that attracts the most visitors');
+  it('should be able to find the dinosaur that attracts the most visitors', function () {
+    const actual = park.findTopDino();
+    assert.deepStrictEqual(actual, dinosaur1);
+  });
 
   it('should be able to find all dinosaurs of a particular species');
 
